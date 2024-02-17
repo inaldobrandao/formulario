8c9182b6c0c9a9ae3343b7662e893d396665c4f0
inaldobrandao <inaldopj@gmail.com>
2024-02-17 12:08:04 -0300

    configure prisma

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -3,6 +3,6 @@ import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
   final handler = startServer();
-  final server = await io.serve(handler, '0.0.0.0', 4444);
+  final server = await io.serve(handler, 'localhost', 4444);
   print('Server online: 0.0.0.0:${server.port}');
 }
