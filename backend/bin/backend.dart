3b23268e4b0da877f1cc5811853388c057e78c75
inaldobrandao <inaldopj@gmail.com>
2024-02-18 00:05:58 -0300

    changing and deleting files

--- backend/bin/backend.dart
+++
@@ -1,8 +0,0 @@
-import 'package:backend/backend.dart';
-import 'package:shelf/shelf_io.dart' as io;
-
-void main(List<String> arguments) async {
-  final handler = startServer();
-  final server = await io.serve(handler, 'localhost', 4444);
-  print('Server online: 0.0.0.0:${server.port}');
-}
